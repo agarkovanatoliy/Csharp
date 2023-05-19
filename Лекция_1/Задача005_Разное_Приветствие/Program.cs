777f5e2add405854f2cde7083f2687de3f74762e
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-19 19:24:02 +0300

    Задача005 разное приветствие

---
+++ Лекция_1/Задача005_Разное_Приветствие/Program.cs
@@ -0,0 +1,14 @@
+﻿//Приветствие разных пользователей по разному
+
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine( );
+
+if(username.ToLower() == "маша")  //ToLower() - команда которая переводит все верхние регистры в нижние
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
