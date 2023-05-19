d2539bed8e443ca52d251986de52c46d183903de
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-19 18:31:57 +0300

    Задача003 Sum Random

--- Лекция_1/Задача003_Sum/Program.cs
+++ Лекция_1/Задача003_Sum/Program.cs
@@ -1,6 +1,16 @@
 ﻿// сложение 2-х заданных чисел
 
-int numberA = 5;
-int numberB = 3;
+// int numberA = 5;
+// int numberB = 3;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+
+// сложение 2-х чисел полученых случайно
+
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
