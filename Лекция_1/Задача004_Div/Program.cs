2fa4d629c7cfd2b22365c33211f87762f870c3b7
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-19 19:10:37 +0300

    Задача004 Div

---
+++ Лекция_1/Задача004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿// деление 2-х заданных чисел
+
+double numberA = 12;    // double - при делении лучше использовать вещественнй тип данных
+double numberB = 7;
+double result = numberA / numberB;
+Console.WriteLine(result);
