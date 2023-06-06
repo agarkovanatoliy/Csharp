cef60bc6ff9f91385db50593ea73465eda6b9c36
AnatoliyAgarkov <tolifander@gmail.com>
2023-06-06 11:55:41 +0300

    Вычисление факториала

---
+++ Лекция_4/факториал/Program.cs
@@ -0,0 +1,13 @@
+﻿// Факториал
+
+
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{i}!={Factorial(i)}");
+}
