7dcf8620003f59306291e6dd942b4ca3aa10d46f
AnatoliyAgarkov <tolifander@gmail.com>
2023-06-06 12:00:13 +0300

    Вычисление числа фибоначи

---
+++ Лекция_4/фибоначи/Program.cs
@@ -0,0 +1,13 @@
+﻿// Фибоначи
+
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{i}!={Fibonacci(i)}");
+}
